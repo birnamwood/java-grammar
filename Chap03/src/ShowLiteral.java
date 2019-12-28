94502d1fb787791ad6b46402639afd9e689e3a39
birnamwood <birnamwood19@gmail.com>
2019-12-28 18:13:42 +0900

    first commit

---
+++ Chap03/src/ShowLiteral.java
@@ -0,0 +1,12 @@
+
+public class ShowLiteral {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("123");
+		System.out.println(456);
+		System.out.println(789.1);
+
+	}
+
+}
---
+++ Chap03/src/UseVariablrd.java
@@ -0,0 +1,16 @@
+
+public class UseVariablrd {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "佐藤";
+		int num = 551;
+		double dec = 67.8;
+
+		System.out.println(name);
+		System.out.println(num);
+		System.out.println(dec);
+
+	}
+
+}
