e1b4c2776c75917f19d80a4bb5ea605184895889
birnamwood <birnamwood19@gmail.com>
2019-12-28 19:38:20 +0900

    4章 条件分岐　開始

---
+++ Chap04/src/IfStatement.java
@@ -0,0 +1,15 @@
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num = 30;
+		if(num <= 40) {
+			System.out.println("数値は40以下です。");
+		} else {
+			System.out.println("数値は40以上です。");
+		}
+
+	}
+
+}
