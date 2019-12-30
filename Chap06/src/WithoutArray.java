45ebbae1fe14d7f2df406d758e8906ed3faa386e
birnamwood <birnamwood19@gmail.com>
2019-12-30 14:31:25 +0900

    6章　配列　開始

---
+++ Chap06/src/WithoutArray.java
@@ -0,0 +1,20 @@
+
+public class WithoutArray {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 8;
+		int num2 = 9;
+		int num3 = 17;
+		int num4 = 26;
+		int num5 = 43;
+
+		System.out.println("num1: " + num1);
+		System.out.println("num2: " + num2);
+		System.out.println("num3: " + num3);
+		System.out.println("num4: " + num4);
+		System.out.println("num5: " + num5);
+
+	}
+
+}
